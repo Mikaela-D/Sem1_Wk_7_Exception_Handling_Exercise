c31b8485e04623478b9f6a1506f8eabc03a61183
Mikaela <G00401392@atu.ie>
2022-11-08 14:19:05 +0000

    Code to handle a mismatch exception

--- src/main/java/ie/atu/week7/UsingTryCatch.java
+++ src/main/java/ie/atu/week7/UsingTryCatch.java
@@ -7,11 +7,22 @@ public class UsingTryCatch {
     public static void main(String[] args) {
         System.out.println("Please enter a number between 0 - 9 : ");
         Scanner myscan = new Scanner(System.in);
-        int myNum = myscan.nextInt();
-        if (myNum <= 9 && myNum > 0) {
-            System.out.println("You entered " + myNum);
-        } else {
-            System.out.println("Not a valid number");
+        char Y = 0;
+        while (Y == 0) {
+            try {
+                int myNum = myscan.nextInt();
+                if (myNum <= 9 && myNum > 0) {
+                    System.out.println("You entered " + myNum);
+                    break;
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error caught\n");
+                System.out.println(e);
+                System.out.println("Please enter a number between 0 - 9 : ");
+                myscan.next();
+            }
         }
     }
 }
